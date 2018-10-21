15f0b4a3ebcc85cb520b78561a752fef03306e2e
amaan uddin <md.amanuddin@gmail.com>
2018-10-21 00:02:17 -0400

    model Created with id input from textbox

--- pathfindermanager.py
+++ pathfindermanager.py
@@ -12,21 +12,39 @@ db=SQLAlchemy(app)
 @app.route("/mgmt",methods=["POST","GET"])
 def home():
 	if request.form:
-		print(request.form)
+		id=request.form.get("id")
+		fn=request.form.get("firstname")
+		ln=request.form.get("lastname")
+		mn=request.form.get("middlename")
+		addr=request.form.get("addr")
+		school=request.form.get("school")
+		fname=request.form.get("fname")
+		phone=request.form.get("phone")
+		batch=request.form.get("batch")
+		dateAdd=request.form.get("dateAdd")
+		optradio=request.form.get("optradio")
+		pathfinder=Pathfinder(id=id,firstname=fn,lastname=ln,middlename=mn,addr=addr,school=school,fname=fname,batch=batch,dateAdd=dateAdd,optradio=optradio)
+		db.session.add(pathfinder)
+		db.session.commit()
+	return render_template("index.html")
+		
+		
 	return render_template("index.html")
 
 class Pathfinder(db.Model):
-	id=db.Column(BigInteger().with_variant(Integer,"sqlite"),primary_key=True)
+	id=db.Column(db.Integer,nullable=False,primary_key=True)
 	firstname=db.Column(db.String(80),nullable=False)
 	lastname=db.Column(db.String(80),nullable=False)
 	middlename=db.Column(db.String(80),nullable=True)
 	addr=db.Column(db.String(80),nullable=False)
 	school=db.Column(db.String(80),nullable=True)
 	fname=db.Column(db.String(80),nullable=True)
-	phone=db.Column(db.String(80),nullable=True)
+	phone=db.Column(db.String(80),nullable=True,)
 	batch=db.Column(db.String(4),nullable=True)
 	dateAdd=db.Column(db.String(10),nullable=False)
 	optradio=db.Column(db.String(10),nullable=False)
+	def __repr__(Self):
+		return "<Name & Phone {} >".format(self.firstname+self.lastname+self.phone)
 	
 	
   
