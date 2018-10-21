23f8c5c0d6cf462f573e0e1f174cb08ab956d45a
amaan uddin <md.amanuddin@gmail.com>
2018-10-20 22:29:51 -0400

    sqlalchemy support added

--- pathfindermanager.py
+++ pathfindermanager.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
+pfile_dir= os.path.dirname(os.path.abspath(__file__))
+db_file="sqlite:///{}".format(os.path.join(pfile_dir,"pathfinder.db"))
 app = Flask(__name__)
+app.config["SQLAlCHEMY_DATABASE_URI"] = db_file
+db=SQLAlchemy(app)
 
 @app.route("/",methods=["POST","GET"])
 def home():
