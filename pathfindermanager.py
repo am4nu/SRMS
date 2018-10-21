6d04666a65b334b26056581a3acec22f7f1fb7a5
amaan uddin <md.amanuddin@gmail.com>
2018-10-21 02:00:21 -0400

    CreateRecordWorking

--- pathfindermanager.py
+++ pathfindermanager.py
@@ -1,13 +1,32 @@
 import os
+
 from flask import Flask
 from flask import render_template
 from flask import request
+
 from flask_sqlalchemy import SQLAlchemy
-pfile_dir= os.path.dirname(os.path.abspath(__file__))
-db_file="sqlite:///{}".format(os.path.join(pfile_dir,"pathfinder.db"))
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "pathfinder.db"))
+
 app = Flask(__name__)
-app.config["SQLAlCHEMY_DATABASE_URI"] = db_file
-db=SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+class Pathfinder(db.Model):
+	id=db.Column(db.Integer,nullable=False,primary_key=True)
+	firstname=db.Column(db.String(80),nullable=False)
+	lastname=db.Column(db.String(80),nullable=False)
+	middlename=db.Column(db.String(80),nullable=True)
+	addr=db.Column(db.String(80),nullable=False)
+	school=db.Column(db.String(80),nullable=True)
+	fname=db.Column(db.String(80),nullable=True)
+	phone=db.Column(db.Integer,nullable=True,)
+	batch=db.Column(db.String(4),nullable=True)
+	dateAdd=db.Column(db.String(10),nullable=False)
+	optradio=db.Column(db.String(10),nullable=False)
+	def __repr__(self):
+		return "<ID & Name & Phone {} >".format(str(self.id)+" "+self.firstname+" "+self.lastname+" "+str(self.phone))
 
 @app.route("/mgmt",methods=["POST","GET"])
 def home():
@@ -23,28 +42,11 @@ def home():
 		batch=request.form.get("batch")
 		dateAdd=request.form.get("dateAdd")
 		optradio=request.form.get("optradio")
-		pathfinder=Pathfinder(id=id,firstname=fn,lastname=ln,middlename=mn,addr=addr,school=school,fname=fname,batch=batch,dateAdd=dateAdd,optradio=optradio)
+		pathfinder=Pathfinder(id=id,firstname=fn,lastname=ln,middlename=mn,addr=addr,phone=phone,school=school,fname=fname,batch=batch,dateAdd=dateAdd,optradio=optradio)
 		db.session.add(pathfinder)
 		db.session.commit()
 	return render_template("index.html")
 		
-		
-	return render_template("index.html")
-
-class Pathfinder(db.Model):
-	id=db.Column(db.Integer,nullable=False,primary_key=True)
-	firstname=db.Column(db.String(80),nullable=False)
-	lastname=db.Column(db.String(80),nullable=False)
-	middlename=db.Column(db.String(80),nullable=True)
-	addr=db.Column(db.String(80),nullable=False)
-	school=db.Column(db.String(80),nullable=True)
-	fname=db.Column(db.String(80),nullable=True)
-	phone=db.Column(db.String(80),nullable=True,)
-	batch=db.Column(db.String(4),nullable=True)
-	dateAdd=db.Column(db.String(10),nullable=False)
-	optradio=db.Column(db.String(10),nullable=False)
-	def __repr__(Self):
-		return "<Name & Phone {} >".format(self.firstname+self.lastname+self.phone)
 	
 	
   
